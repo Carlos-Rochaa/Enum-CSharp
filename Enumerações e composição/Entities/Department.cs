05bea08da0fadc4d99601c19a248371060c023f7
Carlos Rocha <carlosrocha1896@gmail.com>
2024-01-17 21:26:54 -0300

    Adicionar arquivos de projeto.

---
+++ Enumerações e composição/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Enumerações_e_composição.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        { 
+
+        }
+
+        public Department(string name) 
+        {
+            Name = name; 
+        }
+    }
+}
---
+++ Enumerações e composição/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Enumerações_e_composição.Entities.Enums
+{
+    enum WorkerLevel: int
+    {
+        Junior = 0, 
+        MidLevel = 1,
+        Senior = 2
+
+    }
+}
